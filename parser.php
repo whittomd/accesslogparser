02524f75452020702eed78cb6a7a269efa7ff00b
Derek Whittom <dwhittom@talentwise.com>
2016-04-21 14:24:55 -0700

    .gitignore

--- parser.php
+++ parser.php
@@ -12,17 +12,36 @@ echo $pattern . PHP_EOL;
 $iterator = new GlobIterator($path . '/' . $pattern);
 $parser = new \Kassner\LogParser\LogParser();
 $parser->setFormat("%V\t%h\t%l\t%u\t%t\t\"%r\"\t%>s\t%b\t%D\t\"%{Referer}i\"\t\"%{User-Agent}i\"\t\"%{PHPSESSID}i\"");
+$items = array();
 foreach($iterator as $fileInfo) {
    //echo $fileInfo->getFilename() . "<br>\n";
    $fileName = $path . '/' . $fileInfo->getFilename();
    echo "$fileName: " . file_exists($fileName) . PHP_EOL;
    $lines = file($fileName, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-   print_r($lines);
+
    foreach ($lines as $line) {
       $entry = $parser->parse($line);
-      print_r($entry);
+      if(!empty($entry['request'])) {
+         if(preg_match("/(.*) (.*) (.*)/U", $entry['request'], $output)) {
+            $uri = $output[2];
+            $url = "http://www.talentwise.com" . $uri;
+            $urlComponents = parse_url($url);
+            $file = $urlComponents['path'];
+            if(preg_match("/^.*\.(jpg|jpeg|png|gif|css|woff|woff2|eot|js)$/i", $file, $outputData)) {
+               if(!empty($outputData[1])) {
+                  $assetName = $outputData[1];
+                  if(!isset($items[$assetName])) {
+                     $items[$assetName] = 0;
+                  }
+                  $items[$assetName]++;
+               }
+            }
+         }
+      }
    }
 }
+
+print_r($items);
 //
 //
 //foreach ($lines as $line) {
