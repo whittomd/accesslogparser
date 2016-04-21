7f5f1f04f7fb7db7c8a00a50fc82895c98476508
Derek Whittom <dwhittom@talentwise.com>
2016-04-21 13:44:06 -0700

    .gitignore

--- parser.php
+++ parser.php
@@ -11,13 +11,13 @@ $pattern = $argv[2];
 echo $pattern . PHP_EOL;
 $iterator = new GlobIterator($path . '/' . $pattern);
 $parser = new \Kassner\LogParser\LogParser();
-$parser->setFormat('%V\t%h\t%l\t%u\t%t\t"%r"\t%>s\t%b\t%D\t"%{Referer}i"\t"%{User-Agent}i"\t"%{PHPSESSID}C"');
+$parser->setFormat("%V\t%h\t%l\t%u\t%t\t\"%r\"\t%>s\t%b\t%D\t\"%{Referer}i\"\t\"%{User-Agent}i\"\t\"%{PHPSESSID}C\"");
 foreach($iterator as $fileInfo) {
    //echo $fileInfo->getFilename() . "<br>\n";
    $fileName = $path . '/' . $fileInfo->getFilename();
    echo "$fileName: " . file_exists($fileName) . PHP_EOL;
    $lines = file($fileName, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
+   print_r($lines);
    foreach ($lines as $line) {
       $entry = $parser->parse($line);
       print_r($entry);
