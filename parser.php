722661111e9efe8bda8f7e7f648fbb76f28236c1
Derek Whittom <dwhittom@talentwise.com>
2016-04-26 12:33:49 -0700

    .gitignore

--- parser.php
+++ parser.php
@@ -37,7 +37,8 @@ foreach($iterator as $fileInfo) {
                   if (!empty($outputData[0])) {
                      $assetName = $outputData[0];
                      $assetNameParts = explode('/', $assetName);
-                     if(in_array($assetNameParts[2], array('templates', 'javascript'))) {
+                     if(in_array($assetNameParts[2], array('templates', 'javascript', 'screening'))) {
+                        array_shift($assetNameParts);
                         array_shift($assetNameParts);
                         $assetName = implode('/', $assetNameParts);
                      }
