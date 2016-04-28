daaef2335d61da6f073f7295060368cf1b34136e
Derek Whittom <dwhittom@talentwise.com>
2016-04-28 10:35:33 -0700

    .gitignore

--- process.php
+++ process.php
@@ -10,6 +10,15 @@ require_once 'vendor/autoload.php';
 $path = $argv[1];
 
 $data = file_get_contents($path);
+if(!file_exists('map.txt')) {
+   touch('map.txt');
+}
+$mapLines = file('map.txt');
+$map = array();
+foreach($mapLines as $line) {
+   list($url, $file) = explode("|", $line);
+   $map[$url] = $file;
+}
 $items = json_decode($data, true);
 $finalItems = array();
 $totalItems = count($items);
@@ -17,15 +26,20 @@ echo "Starting to process $totalItems items\n";
 $index = 0;
 foreach($items as $url => $count) {
    $index++;
-   $file = file_get_contents($url);
-   if(empty($file)) {
-      $url = str_replace(array('.min', 'minified/'), array('', ''), $url);
+   if (empty($map[$url])) {
       $file = file_get_contents($url);
+      if (empty($file)) {
+         $url = str_replace(array('.min', 'minified/'), array('', ''), $url);
+         $file = file_get_contents($url);
+      }
+      $map[$url] = $file;
+      file_put_contents('map.txt', "$url|$file" . PHP_EOL, FILE_APPEND);
+   } else {
+      $file = $map[$url];
    }
    echo "Item $index/$totalItems: Working: $url => $file\n";
 
    $finalItems[$file]['urls'][$url] = $count;
+   $finalItemsJson = json_encode($finalItems);
+   file_put_contents('finaloutput.json', $finalItemsJson);
 }
-
-$finalItemsJson = json_encode($finalItems, JSON_PRETTY_PRINT);
-file_put_contents('finaloutput.json', $finalItemsJson);
